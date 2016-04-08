1eb76718573228d00af66168b8d0e67c2dd92513
imRishabhGupta <rishabhgpt86@gmail.com>
2016-04-09 00:03:14 +0530

    Contact ui update

--- app/src/main/java/com/example/user/dtuapp/ContactActivity.java
+++ app/src/main/java/com/example/user/dtuapp/ContactActivity.java
@@ -2,12 +2,9 @@ package com.example.user.dtuapp;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.transition.Slide;
 import android.view.Gravity;
-import android.view.View;
 import android.view.animation.AnimationUtils;
 
 public class ContactActivity extends AppCompatActivity {
@@ -17,15 +14,6 @@ public class ContactActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
 
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP) {
             Slide slide = new Slide(Gravity.BOTTOM);
