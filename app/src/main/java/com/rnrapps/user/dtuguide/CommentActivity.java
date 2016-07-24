9229c3577e49655bce4a9de45e4ef0f9ba8adc7f
imRishabhGupta <rishabhgpt86@gmail.com>
2016-07-24 17:34:07 +0530

    comments added in ui_changed

---
+++ app/src/main/java/com/rnrapps/user/dtuguide/CommentActivity.java
@@ -0,0 +1,105 @@
+package com.rnrapps.user.dtuguide;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.android.volley.toolbox.ImageLoader;
+import com.rnrapps.user.dtuguide.Utils;
+
+import java.util.ArrayList;
+
+public class CommentActivity extends AppCompatActivity {
+
+    private CommentsAdapter commentsAdapter;
+    private ImageLoader imageLoader = AppController.getInstance().getImageLoader();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+            }
+        });
+
+        final ListView commentListView=(ListView)findViewById(R.id.commentslist);
+        TextView timestamp = (TextView)findViewById(R.id.timestamp);
+        TextView statusMsg = (TextView)findViewById(R.id.txtStatusMsg);
+        TextView url = (TextView)findViewById(R.id.txtUrl);
+
+        FeedImageView feedImageView = (FeedImageView)findViewById(R.id.feedImage1);
+
+        ArrayList<CommentItem> commentItems;
+        FeedItem feedItem;
+        commentItems=new ArrayList<>();
+        Bundle bundle=getIntent().getExtras();
+        if (bundle.getParcelableArrayList("comments")!=null)
+           commentItems=bundle.getParcelableArrayList("comments");
+        feedItem=bundle.getParcelable("status");
+
+        // Converting timestamp into x ago format
+        String a=feedItem.getTimeStamp();
+        timestamp.setText(Utils.getTimeFromTimestamp(a));
+
+        // Check for empty status message
+        if (!TextUtils.isEmpty(feedItem.getStatus())) {
+            statusMsg.setText(feedItem.getStatus());
+            statusMsg.setVisibility(View.VISIBLE);
+        } else {
+            // status is empty, remove from view
+            statusMsg.setVisibility(View.GONE);
+        }
+
+        // Checking for null feed url
+        if (feedItem.getUrl() != null) {
+            url.setText(Html.fromHtml("<a href=\"" + feedItem.getUrl() + "\">"
+                    + feedItem.getUrl() + "</a> "));
+
+            // Making url clickable
+            url.setMovementMethod(LinkMovementMethod.getInstance());
+            url.setVisibility(View.VISIBLE);
+        } else {
+            // url is null, remove from the view
+            url.setVisibility(View.GONE);
+        }
+
+        // Feed image
+        if (feedItem.getImge() != null) {
+            feedImageView.setImageUrl(feedItem.getImge(), imageLoader);
+            feedImageView.setVisibility(View.VISIBLE);
+            feedImageView
+                    .setResponseObserver(new FeedImageView.ResponseObserver() {
+                        @Override
+                        public void onError() {
+                        }
+
+                        @Override
+                        public void onSuccess() {
+                        }
+                    });
+        } else {
+            feedImageView.setVisibility(View.GONE);
+        }
+
+        if(commentItems.size()!=0) {
+            commentsAdapter = new CommentsAdapter(this, commentItems);
+            commentsAdapter.notifyDataSetChanged();
+            commentListView.setAdapter(commentsAdapter);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/rnrapps/user/dtuguide/Utils.java
@@ -0,0 +1,34 @@
+package com.rnrapps.user.dtuguide;
+
+import android.text.format.DateUtils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by rohanagarwal94 on 20/7/16.
+ */
+public class Utils {
+    private Utils() {
+    }
+
+    public static String getTimeFromTimestamp(String a)
+    {
+        String timeAgo=null;
+        a=a.replace("T"," ").replace("+",".");
+
+        SimpleDateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        try {
+            Date timeCreatedDate = dateFormat.parse(a);
+            timeAgo =(String) DateUtils.getRelativeTimeSpanString(
+                    (timeCreatedDate.getTime()),
+                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return timeAgo;
+    }
+
+}
